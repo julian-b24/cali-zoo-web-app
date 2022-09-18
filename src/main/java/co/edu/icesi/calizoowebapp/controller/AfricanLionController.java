d55984a7389c9aadd89f64652aec2284e47d0a48
julian-b24 <julian.camilob24@gmail.com>
2022-09-17 19:33:22 -0500

    Controller validations in createLion added

--- src/main/java/co/edu/icesi/calizoowebapp/controller/AfricanLionController.java
+++ src/main/java/co/edu/icesi/calizoowebapp/controller/AfricanLionController.java
@@ -3,31 +3,62 @@ package co.edu.icesi.calizoowebapp.controller;
 import co.edu.icesi.calizoowebapp.api.AfricanLionAPI;
 import co.edu.icesi.calizoowebapp.dto.AfricanLionDTO;
 import co.edu.icesi.calizoowebapp.mapper.AfricanLionMapper;
-import co.edu.icesi.calizoowebapp.repository.AfricanLionRespository;
+import co.edu.icesi.calizoowebapp.service.AfricanLionService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @NoArgsConstructor
 public class AfricanLionController implements AfricanLionAPI {
 
-    //Add service field
+    private AfricanLionService africanLionService;
     private AfricanLionMapper africanLionMapper;
 
     @Override
     public AfricanLionDTO getLion(String lionName) {
-        return null;
+        return africanLionMapper.fromAfricanLion(africanLionService.getLion(lionName));
     }
 
     @Override
     public AfricanLionDTO createLion(AfricanLionDTO africanLionDTO) {
-        return null;
+        validateAfricanLionFields(africanLionDTO);
+        return africanLionMapper.fromAfricanLion(africanLionService.createLion(africanLionMapper.fromAfricanLionDTO(africanLionDTO)));
     }
 
     @Override
     public List<AfricanLionDTO> getLions() {
-        return null;
+        return africanLionService.getLions().stream().map(africanLionMapper::fromAfricanLion).collect(Collectors.toList());
+    }
+
+    private void validateAfricanLionFields(AfricanLionDTO africanLionDTO) {
+        validateAfricanLionName(africanLionDTO.getName());
+        validateAfricanLionArrivedZooDate(africanLionDTO.getArrivedZooDate());
+    }
+
+    private void validateAfricanLionArrivedZooDate(LocalDateTime arrivedZooDate) {
+        if(arrivedZooDate.isAfter(LocalDateTime.now())){
+            throw new RuntimeException();
+        }
+    }
+
+    private void validateAfricanLionName(String lionName) {
+        validateAfricanLionNameSize(lionName);
+        validateAfricanLionNameEspecialChars(lionName);
+    }
+
+    private void validateAfricanLionNameEspecialChars(String lionName) {
+        if(!lionName.matches("[\\sa-zA-Z]+")){
+            throw new RuntimeException();
+        }
+    }
+
+    private void validateAfricanLionNameSize(String lionName) {
+        if(lionName.length() > 120){
+            throw new RuntimeException();
+        }
     }
 }
