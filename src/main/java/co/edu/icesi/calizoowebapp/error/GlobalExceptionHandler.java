373eac9e5273f5e9b5077bd1c5bac6198588ecaa
julian-b24 <julian.camilob24@gmail.com>
2022-09-17 20:06:41 -0500

    Exceptions, error and it's codes added

---
+++ src/main/java/co/edu/icesi/calizoowebapp/error/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package co.edu.icesi.calizoowebapp.error;
+
+import co.edu.icesi.calizoowebapp.error.exception.AfricanLionError;
+import co.edu.icesi.calizoowebapp.error.exception.AfricanLionException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<AfricanLionError> handleException(AfricanLionException africanLionException){
+        return new ResponseEntity<>(africanLionException.getError(), africanLionException.getHttpStatus());
+    }
+}
